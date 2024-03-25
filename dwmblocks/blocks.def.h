14b72c5dd170981206f17585ff43db148a8b6f98
Duarte Pedro Pinto <dpnpinto@gmail.com>
2024-03-25 11:31:36 -0100

    organiza altera local dos scrips

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/	 	                            /*Update Interval*/	/*Update Signal*/
-    {" 🐧 ", "/etc/dtos/.config/dwmblocks/scripts/kernel",	  360,		          2},
+    {" 🐧 ", "~/.config/dwmblocks/scripts/kernel",	  360,		          2},
 
-	{" 🔺 ", "/etc/dtos/.config/dwmblocks/scripts/upt",		  60,		          2},
+	{" 🔺 ", "~/.config/dwmblocks/scripts/upt",		  60,		          2},
 
-	{" 📦 ", "/etc/dtos/.config/dwmblocks/scripts/pacupdate", 360,		          9},
+	{" 📦 ", "~/.config/dwmblocks/scripts/pacupdate", 360,		          9},
 	
-	{" 💻 ", "/etc/dtos/.config/dwmblocks/scripts/memory",	  6,		          1},
+	{" 💻 ", "~/.config/dwmblocks/scripts/memory",	  6,		          1},
 
-	{" 🔊 ", "/etc/dtos/.config/dwmblocks/scripts/volume",    0,		          10},
+	{" 🔊 ", "~/.config/dwmblocks/scripts/volume",    0,		          10},
 
-	{" 🕑 ", "/etc/dtos/.config/dwmblocks/scripts/clock",	  60,	              0},
+	{" 🕑 ", "~/.config/dwmblocks/scripts/clock",	  60,	              0},
 
 };
 
